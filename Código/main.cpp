617390066c724c619cd69b41f1a072c6901896fc
Jéssica Pereira <jessica.pereira1764@gmail.com>
2016-05-15 12:41:33 -0300

    Software 0.2 - Funcionamento básico

--- Código/main.cpp
+++ Código/main.cpp
@@ -2,31 +2,31 @@
 #include <stdio.h>
 #include <string>
 #include <locale.h> 	//Biblioteca para configurações regionais
+#include <stdlib.h>
 #include "GerarDiscurso.cpp"
 
 using namespace std; 
 
 void exibirMenu (){
+	int opc=-1;
+	int n = 0, m = 0, res = 0;
+	char back;
+	GerarDiscurso discurso;
+	
+	setlocale(LC_ALL,"");	
+	system("clear");
+
 	printf("\n\tSoftware de Produção de Discursos\n\n");
 	printf("Digite o número correspondente a opção desejada.\n");
 	printf("1. Gerar Discurso\n");
 	printf("2. Consultar discursos armazenados em histórico\n");
 	printf("0. Sair\n");
 	printf("Opção: ");
-}
-
-
-int main() {
-	int opc=-1;
-	int n = 0, m = 0, res = 0;
-	GerarDiscurso discurso;
-	setlocale(LC_ALL,"");
-	
-	exibirMenu();
 	scanf("%d", &opc);
-	
+
 	switch (opc){
 		case 1: 
+			system("clear");
 			printf("\n\tGerar Discurso\n\n");
 			printf("Informe o número de parágrafos: ");
 			scanf("%d", &n);
@@ -43,24 +43,35 @@ int main() {
 				case 1:
 					res = discurso.salvar();
 					if(res == 0)
-						printf("Discurso salvo com sucesso! Para consultá-lo, acesse o ítem 2 no menu\n");
+						printf("Discurso salvo com sucesso! Para consultá-lo, acesse o ítem 2 no menu.\n\n");
 					else
 						printf("Erro ao salvar o discurso.\n");
-					printf("Retornar ao menu.\n");
+					printf("Precione qualquer tecla para voltar ao menu: ");
+					getchar();
+					scanf("%c", &back);
+					exibirMenu ();
 					break;
 				case 2:
 					res = discurso.apagarTmp();
 					if(res != 0)
 						printf("Erro ao apagar arquivo temporário.\n");
-					printf("Retornar ao menu.\n");
+					printf("Precione qualquer tecla para voltar ao menu: ");
+					getchar();
+					scanf("%c", &back);
+					exibirMenu ();
 					break;
 				default:
 					printf("Número inválido.\n");
+					printf("Precione qualquer tecla para voltar ao menu: ");
+					getchar();
+					scanf("%c", &back);
+					exibirMenu ();
 					break;
 			}
 			break;
 
 		case 2:
+			system("clear");
 			printf("\n\tHistórico de Discursos\n\n");
 			printf("Digite o número correspondente ao discurso que deseja consultar:\n");
 			discurso.listarSalvos();
@@ -69,17 +80,32 @@ int main() {
 			res = discurso.consultar(m);
 			if(res != 0)
 				printf("Erro ao acessar discurso.\n");
-			printf("Retornar ao menu.\n");
+			printf("Precione qualquer tecla para voltar ao menu: ");
+			getchar();
+			scanf("%c", &back);
+			exibirMenu ();
 			break;
 
 		case 0:
-			return 0;
+			printf("Tem certeza que deseja sair do programa?\n[N]. Não, voltar ao menu\n[S]. Sim\nOpção: ");
+			getchar();
+			scanf("%c", &back);
+			if ((back == 'S') || (back == 's'))
+				exit(0);
+			else
+				exibirMenu();
 		default:
 			printf("Número inválido.\n");
+			exibirMenu ();
 			break;
 	}
+}
 
-	
+
+int main() {
+
+	exibirMenu();
+	return 0;
 	//system("pause");
 }
 
